ae0c36bc591854a1f1f6e6025a25b743160af752
Alessandra Nishikawa <alessandranishikawa@imacdeaessandra.local>
2017-10-25 12:21:22 -0400

    Initial Commit

---
+++ VideoRioPoker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoRioPoker
+//
+//  Created by Alessandra Nishikawa on 17-10-25.
+//  Copyright © 2017 Alessandra Nishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
